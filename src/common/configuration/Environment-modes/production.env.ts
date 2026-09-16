12c6d0526ff1689c51a683c7b2659be61e7f85d8
Taha.Elbanna <elbannataha@students.du.edu.eg>
2026-09-16 23:34:25 +0300

    .

--- src/common/configuration/Environment-modes/production.env.ts
+++ src/common/configuration/Environment-modes/production.env.ts
@@ -3,7 +3,6 @@ import { defaultEnv } from './default.env';
 
 export const productionEnv = (): EnvironmentInterface => ({
     ...defaultEnv(),
-    port: 5000,
     accessTokenExpireIn: '30m',
     refreshTokenExpireIn: '30d',
 });
