36d7874d39ec8380eab7ea20224afb71055e3ef5
Taha.Elbanna <elbannataha@students.du.edu.eg>
2026-09-17 14:21:49 +0300

    fix: bind server to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
         credentials: true,
     });
 
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
     logger.log(`Application is running on: http://localhost:${port}`);
 }
 
